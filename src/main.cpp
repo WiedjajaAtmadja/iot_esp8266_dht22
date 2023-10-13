7a67a3d8346a53e540725e1f4cc708fbd112d431
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-10-13 16:44:37 +0700

    step 2. Add DHT device

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,42 @@
-#include <Arduino.h>
+/*
+Device: 
+  - DHT11 or DHT22: Data to D7, VCC to 3.3V, GND to GND
 
+Libraries:
+  - DHT sensor library for ESPx  
+
+*/
+#include <Arduino.h>
+#include "DHTesp.h"
+
+DHTesp dht;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  dht.setup(D7, DHTesp::DHT22);
   Serial.println("System started");
 }
 
 void loop() {
+  delay(dht.getMinimumSamplingPeriod());
+
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+  Serial.print(dht.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.print(dht.toFahrenheit(temperature), 1);
+  Serial.print("\t\t");
+  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
+  Serial.print("\t\t");
+  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+
+  // delay(2000);
   Serial.println("Hello World");
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
